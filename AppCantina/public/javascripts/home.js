007bc9c595ef6807d48e6a549b7af25a7714a725
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2023-04-04 15:39:53 +0100

    botoes resolvidos :japanese_castle:

--- AppCantina/public/javascripts/home.js
+++ AppCantina/public/javascripts/home.js
@@ -1,7 +1,33 @@
 $(function(){
-    $(function(){
-        $("#dropdown-btn").click(function(){
-          $(".dropdown-menu").toggleClass("show");
-        });
-      });  
+ 
 })
+
+function showImage(fname, ftype){
+  if((ftype=='image/png')||(ftype=='image/jpeg')){
+      var ficheiro = $('<img src="/fileStore/'+f.name+ '"width ="80%"/>')
+      var download = $('<div><a href="/download/'+f.name+ '>Download</a></div>')
+      $("#display").empty()
+      $("#display").append(ficheiro,download)
+      $("#display").modal()
+  }
+  else if(ftype=='application/json')
+      $.get('/fileContents/'+fname, function(response){
+          var contents = response
+          var ficheiro=$('<pre>'+ contents +'</pre>')
+          var download = $('<div><a href="/download/'+f.name+ '>Download</a></div>')
+          $("#display").empty()
+          $("#display").append(ficheiro,download)
+          $("#display").modal()
+      })
+      .fail(function(error){
+          console.log(error)
+      })
+  else{
+      var ficheiro = $('<p>'+fname +'</p>')
+      var download = $('<div><a href="/download/'+f.name+ '>Download</a></div>')
+      $("#display").empty()
+      $("#display").append(ficheiro,download)
+      $("#display").modal()
+  }
+  
+}
