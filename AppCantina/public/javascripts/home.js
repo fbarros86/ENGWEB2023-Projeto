3530c664a5e2a8afc312ad5d390349b9b091a3f0
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2023-04-06 17:50:55 +0100

    home page 🦖

--- AppCantina/public/javascripts/home.js
+++ AppCantina/public/javascripts/home.js
@@ -2,32 +2,9 @@ $(function(){
  
 })
 
-function showImage(fname, ftype){
-  if((ftype=='image/png')||(ftype=='image/jpeg')){
-      var ficheiro = $('<img src="/fileStore/'+f.name+ '"width ="80%"/>')
-      var download = $('<div><a href="/download/'+f.name+ '>Download</a></div>')
-      $("#display").empty()
-      $("#display").append(ficheiro,download)
-      $("#display").modal()
-  }
-  else if(ftype=='application/json')
-      $.get('/fileContents/'+fname, function(response){
-          var contents = response
-          var ficheiro=$('<pre>'+ contents +'</pre>')
-          var download = $('<div><a href="/download/'+f.name+ '>Download</a></div>')
-          $("#display").empty()
-          $("#display").append(ficheiro,download)
-          $("#display").modal()
-      })
-      .fail(function(error){
-          console.log(error)
-      })
-  else{
-      var ficheiro = $('<p>'+fname +'</p>')
-      var download = $('<div><a href="/download/'+f.name+ '>Download</a></div>')
-      $("#display").empty()
-      $("#display").append(ficheiro,download)
-      $("#display").modal()
-  }
-  
+function showContent(day){
+    var ficheiro=$('<pre>'+ day +'</pre>')
+    $("#display").empty()
+    $("#display").append(ficheiro)
+    $("#display").modal() 
 }
