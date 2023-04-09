4aac16a433c3ba7e17da4242adb1eaa514d66f48
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2023-04-09 18:12:14 +0100

    cenas bue da fixes

--- AppCantina/public/javascripts/home.js
+++ AppCantina/public/javascripts/home.js
@@ -10,5 +10,33 @@ function showContent(day){
 }
 
 function selectDay(element){
-    element.classList.toggle('selected');
+    const senhas = document.querySelector('.senhas');
+    element.classList.toggle('selected'); 
+    if (element.classList.contains('selected')) {
+        senhas.textContent = Number(senhas.textContent) - 1;
+      } else {
+        senhas.textContent = Number(senhas.textContent) + 1;
+      }
+      
+    const selectedSquares = document.querySelectorAll('.selected');
+     if (selectedSquares.length == 0) {
+        senhas.classList.remove('areservar');
+    }else{
+        senhas.classList.add('areservar');
+    }
+
+}
+
+function reservou(){
+    const senhas = document.querySelector('.senhas');
+    const selectedSquares = document.querySelectorAll('.selected');
+    selectedSquares.forEach(square =>{
+        square.classList.remove('selected')
+    })
+    senhas.classList.remove('areservar');
+    senhas.classList.add('reservou');
+    setTimeout(function(){
+        senhas.classList.remove('reservou')
+    }, 1500);
+
 }
