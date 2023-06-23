492d047ef58cadf420a945135369f02ec98c4c4b
fbarros86 <xiquita.barros@gmail.com>
2023-06-23 09:54:37 +0100

    Adicionei funcao apagada

--- AppCantina/public/javascripts/form.js
+++ AppCantina/public/javascripts/form.js
@@ -48,3 +48,20 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+function deleteUser(idU){
+  $.ajax({
+      url:"http://localhost:7778/users/"+idU,
+      type: 'DELETE',
+      success: function(response) {
+        // Handle the successful response
+        window.location.href ="http://localhost:7777/form"
+      },
+      error: function(xhr, status, error) {
+        // Handle the error
+        console.log(error);
+      }
+    });
+  
+}
